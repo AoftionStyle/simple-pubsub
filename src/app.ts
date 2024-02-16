2467f11b3d182670593a2dfd8202d17fb5c0ab8a
AOFTION <chutipon@amity.co>
2024-02-16 17:32:04 +0700

    update src/app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,6 @@
-import { Machine } from "./model/Machine";
-import { IEvent, MachineRefillEvent, MachineSaleEvent } from "./service/Event";
-import { IPublishSubscribeService, PublishSubscribeService } from "./service/pubsub/PublishSubscribeService";
-import { MachineRefillSubscriber, MachineSaleSubscriber, StockWarningSubscriber } from "./service/pubsub/Subscriber";
+import { MachineController } from "./machine/MachineController";
+import { MachineRefillEvent, MachineSaleEvent } from "./machine/MachineEvent";
+import { IEvent } from "./pubsub/IEvent";
 
 
 // helpers
@@ -16,7 +15,7 @@ const randomMachine = (): string => {
 
 }
 
-const eventGenerator = (): IEvent => {
+export const eventGenerator = (): IEvent => {
   const random = Math.random();
   if (random < 0.5) {
     const saleQty = Math.random() < 0.5 ? 1 : 2; // 1 or 2
@@ -29,34 +28,6 @@ const eventGenerator = (): IEvent => {
 
 // program
 (async () => {
-  // create 3 machines with a quantity of 10 stock
-  const machines: Machine[] = [ new Machine('000'), new Machine('001'), new Machine('002'), new Machine('003') ];
-
-  // create a machine sale event subscriber. inject the machines (all subscribers should do this)
-  const saleSubscriber = new MachineSaleSubscriber(machines);
-  const refillSubscriber = new MachineRefillSubscriber(machines);
-  const stockWarningSubscriber = new StockWarningSubscriber(machines);
-
-  // create the PubSub service
-  // const pubSubService: IPublishSubscribeService = null as unknown as IPublishSubscribeService; // implement and fix this
-  const pubSubService: IPublishSubscribeService = new PublishSubscribeService(machines);
-  pubSubService.subscribe(saleSubscriber);
-  pubSubService.subscribe(refillSubscriber);
-  pubSubService.subscribe(stockWarningSubscriber);
-
-  // create 5 random events
-  const events = [1,2,3,4,5].map(i => eventGenerator());
-  events.forEach(event => {
-    // if (event instanceof MachineSaleEvent) {
-    //   pubSubService.subscribe(event.type(), saleSubscriber);
-    // } else if (event instanceof MachineRefillEvent) {
-    //   pubSubService.subscribe(event.type(), refillSubscriber);
-    // }
-    // pubSubService.subscribe(event.type(), stockWarningSubscriber);
-
-    
-  })
-
-  // publish the events
-  events.map(pubSubService.publish);
+  const machineController = new MachineController();
+  machineController.publish();
 })();
