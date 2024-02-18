121b3ec96e01df6da9780437a4b40d12aa47c6a4
AOFTION <chutipon@amity.co>
2024-02-19 04:03:58 +0700

    create abstract class MachineSubscriber apply to other Subscirber as same behavior signature constructor

--- src/machine/MachineSubscriber.ts
+++ src/machine/MachineSubscriber.ts
@@ -1,51 +1,45 @@
-import { IEvent } from "../pubsub/IEvent";
-import { ISubscriber } from "../pubsub/ISubscriber";
+import { IEvent } from "../utils/pubsub/IEvent";
+import { ISubscriber } from "../utils/pubsub/ISubscriber";
 import { Machine } from "./Machine";
 import { MachineRefillEvent, MachineSaleEvent } from "./MachineEvent";
 
-
-// update handle(event: MachineSaleEvent) with use event.machineId() as position machines[]
-// to decresing stockLevel by event.getSoldQuantity()
-export class MachineSaleSubscriber implements ISubscriber {
-  public machines: Machine[];
-
-  constructor (machines: Machine[]) {
-    this.machines = machines; 
+export abstract class MachineSubscriber implements ISubscriber {
+  constructor (protected machines: Machine[]) {
+    this.machines = machines;
   }
 
+  handle(event: IEvent): void {
+    throw new Error("Method not implemented.");
+  }
+}
+
+export class MachineSaleSubscriber extends MachineSubscriber {
+  // update handle(event: MachineSaleEvent) with use event.machineId() as position machines[]
+  // overriding super class method
   handle(event: MachineSaleEvent): void {
+    // to decresing stockLevel by event.getSoldQuantity()
     this.machines[Number(event.machineId())].stockLevel -= event.getSoldQuantity();
   }
 }
 
-// update handle(event: MachineRefillEvent) with use event.machineId as position machines[]
-// to increasing stockLevel by event.getRefillQuantity()
-export class MachineRefillSubscriber implements ISubscriber {
-  public machines: Machine[];
-
-  constructor (machines: Machine[]) {
-    this.machines = machines; 
-  }
-
+export class MachineRefillSubscriber extends MachineSubscriber {
+  // update handle(event: MachineRefillEvent) with use event.machineId as position machines[]
+  // overriding super class method
   handle(event: MachineRefillEvent): void {
+    // to increasing stockLevel by event.getRefillQuantity()
     this.machines[Number(event.machineId())].stockLevel += event.getRefillQuantity();
   }
 }
 
 /**
- * create class StockWarningSubscriber with implement ISubscriber
+ * create class StockWarningSubscriber
  * handle(event: IEvent) covered by MachineSaleEvent and MahcineRefillEvent
- * if a machine stock levels drops below 3 a new Event, LowStockWarningEvent should be generated. 
- * when the stock levels hits 3 or above (because of a MachineRefillEvent), a StockLevelOkEvent should be generated.
- */
-export class StockWarningSubscriber implements ISubscriber {
-  public machines: Machine[];
-
-  constructor(machines: Machine[]) {
-    this.machines = machines;
-  }
-
-  handle(event: IEvent): void {
+*/
+export class MachineStockWarningSubscriber extends MachineSubscriber {
+  // if a machine stock levels drops below 3 a new Event, LowStockWarningEvent should be generated. 
+  // when the stock levels hits 3 or above (because of a MachineRefillEvent), a StockLevelOkEvent should be generated.
+  // overriding super class method
+  public handle(event: IEvent): void {
     if (event instanceof MachineRefillEvent && this.machines[Number(event.machineId())].stockLevel >= 3) {
       this.machines[Number(event.machineId())].stockStatus = 'StockLevelOkEvent';
     } else if (this.machines[Number(event.machineId())].stockLevel < 3) {
